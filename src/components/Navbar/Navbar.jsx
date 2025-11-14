f45ecc3901904b8a10ea9708976d1b6ee4c46a33
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-14 17:36:07 -0500

    Asset template imported. Set up project folder/file structure in components and pages.  Some css done for navbar, continue here.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Navbar.css'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import arrowdown_icon from '../../assets/arrowdown_icon.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navbarLeft'>
+            <h1 className='logo'>TrailerHUB</h1> 
+            <ul>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>What's Popular</li>
+                <li>My Watch List</li>
+                <li>Browse</li>
+            </ul>
+        </div>
+        <div className='navbarRight'>
+            <img className='icons' src={search_icon} alt='Search or Magnifying glass Icon' />
+            <p>Kids</p>
+            <img className='icons' src={bell_icon} alt='Notification or Bell Icon' />
+            <div className='navbarProfile'>
+                <img className='profile' src={profile_img} alt='image of profile picture' />
+                <img className='icons' src={arrowdown_icon} alt='Arrow down or expand Icon' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
