990c7d011c07f483b901a354e37b0c7c6adc5bd0
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-15 02:16:08 -0500

    Navbar styling complete. Hero banner section in progress. Continue on home page stlyling, specifically hero section buttons.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,13 @@ import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from '../../assets/profile_img.png'
 import arrowdown_icon from '../../assets/arrowdown_icon.svg'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='navbarLeft'>
-            <h1 className='logo'>TrailerHUB</h1> 
+            <img src={logo} alt='picture of logo "TrailerHUB"' /> 
             <ul>
                 <li>Home</li>
                 <li>TV Shows</li>
@@ -26,6 +27,11 @@ const Navbar = () => {
             <div className='navbarProfile'>
                 <img className='profile' src={profile_img} alt='image of profile picture' />
                 <img className='icons' src={arrowdown_icon} alt='Arrow down or expand Icon' />
+                <div className='dropdown'>
+                    <p>
+                        Sign out of TrailerHUB
+                    </p>
+                </div>
             </div>
         </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar />
+        <div className='hero'>
+          <img src={hero_banner} 
+          alt='image of movie or show banner' 
+          className='bannerImg'/>
+          <div className='heroCaption'>
+            <img src={hero_title} 
+            alt='image of title of movie' 
+            className='captionImg'/>
+            <p>
+              Shrek's world gets turned upside down 
+              after he makes a deal with Rumpelstiltskin. 
+              Can Donkey, Fiona and Puss in Boots help 
+              him make things right?
+            </p>
+            <div className='heroBtns'>
+              <button className='btn darkBtn'><img src={play_icon} alt='play icon' />Play</button>
+              <button className='btn darkBtn'><img src={info_icon} alt='info icon' />More Info</button>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
