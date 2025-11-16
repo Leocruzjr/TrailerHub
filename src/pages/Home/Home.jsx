f913e612fe4999b98e6a78177eddeec17ee8a443
Leocruzjr <leonardocruzprimary@gmail.com>
2025-11-16 02:58:00 -0500

    Complete styling for hero banner section. Implementing movie card carousels for different collections.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import TitleCards from '../../components/TitleCards/TitleCards'
+
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
@@ -25,11 +28,12 @@ const Home = () => {
               him make things right?
             </p>
             <div className='heroBtns'>
-              <button className='btn darkBtn'><img src={play_icon} alt='play icon' />Play</button>
+              <button className='btn'><img src={play_icon} alt='play icon' />Play</button>
               <button className='btn darkBtn'><img src={info_icon} alt='info icon' />More Info</button>
             </div>
           </div>
         </div>
+        <TitleCards />
     </div>
   )
 }
