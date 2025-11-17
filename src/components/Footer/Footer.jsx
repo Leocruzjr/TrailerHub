16d979246996b44f8ed343ae41b272a37d321db2
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-17 14:09:17 -0500

    + Smooth scroll to index.css. Added footer section with styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import './Footer.css'
-import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assets/instagram_icon.png'
-import facebook_icon from '../../assets/facebook_icon.png'
-
-
-
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h2>TrailerHUB</h2>
+    <footer className='footer'>
       <div className="footerLeft">
-        <h3>About</h3>
-        <p>Thank you for visiting TrailerHUB. 
-          This was created as a personal project with
+        <h2>TrailerHUB</h2>
+        <p>Thank you for visiting. 
+          This was created as a personal project on VSCode with
            React JS & Firebase + TMDB API. </p>
       </div>
       <div className="footerRight">
+        <div className="content">
         <h3>Resources</h3>
         <ul>
           <li>
@@ -34,10 +27,16 @@ const Footer = () => {
             TMDB API
             </a>
           </li>
-
+          <li><a target="_blank" 
+          rel="noopener noreferrer" 
+          href="https://www.netflix.com/">
+            Design Inspo: netflix.com
+            </a>
+          </li>
         </ul>
+        </div>
       </div>
-    </div>
+    </footer>
   )
 }
 
