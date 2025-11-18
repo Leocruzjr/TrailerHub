4c29688a2d7f0371206eb033772ac941eb31b318
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-18 15:39:57 -0500

    small commit with more elements added to login page. Some CSS added as well.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className='login'>
+      <img src={logo} className='loginLogo' alt='' />
+      <div className="loginForm">
+         <h1>Sign Up</h1>
+         <form>
+          <input type='text' placeholder='your name' />
+          <input type='email' placeholder='your email' />
+          <input type='password' placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="formHelp">
+            <div className="remember">
+              <input type='checkbox' />
+              <label htmlFor=''>Remember Me</label>
+            </div>
+            <p>Stuck?</p>
+          </div>
+         </form>
+      </div>
     </div>
   )
 }
