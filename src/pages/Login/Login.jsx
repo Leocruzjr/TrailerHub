077c2faeab6a55c574c0c6db80e4c6d4fa868d48
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-19 03:52:38 -0500

    Login page styling complete and useState implemented. Login page makes a few changes depending on if user is on Sign up OR sign In. NEXT: Integrate TMDB API for titleCards.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In")
   return (
     <div className='login'>
       <img src={logo} className='loginLogo' alt='' />
       <div className="loginForm">
-         <h1>Sign Up</h1>
+         <h1>{signState}</h1>
          <form>
-          <input type='text' placeholder='your name' />
+          {/*If useState equals "sign up"
+           then name input field will show, 
+           else, will remain empty*/}
+          {signState==='Sign Up' ? 
+            <input type='text' placeholder='your name' /> :<></>}
           <input type='email' placeholder='your email' />
           <input type='password' placeholder='Password' />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="formHelp">
             <div className="remember">
               <input type='checkbox' />
@@ -21,6 +26,17 @@ const Login = () => {
             <p>Stuck?</p>
           </div>
          </form>
+         <div className="formSwitch">
+          {signState === 'Sign In' ? 
+            <p>New to TrailerHUB? <span onClick={()=>{setSignState('Sign Up')}}>
+              Sign Up Now</span>
+            </p> 
+            : 
+            <p>Already have an account? <span onClick={()=>{setSignState('Sign In')}}>
+              Sign In Now</span>
+            </p>
+          }
+         </div>
       </div>
     </div>
   )
